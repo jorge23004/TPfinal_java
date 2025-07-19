f16209282999c43a1e9ba14c33fb6ed2f4b04145
jorge23004 <133065372+jorge23004@users.noreply.github.com>
2025-07-19 10:51:49 -0300

    Add files via upload

---
+++ MiPrimerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TalentoTech.miPrimerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
